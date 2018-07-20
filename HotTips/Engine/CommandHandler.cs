665385b0fdaa4fe072bfc60380f826edd368eaa9
Amadeusz Wieczorek <git@amadeusw.com>
2018-07-20 08:46:23 -0700

    makes the MEF expor work, adds comments

--- HotTips/Engine/CommandHandler.cs
+++ HotTips/Engine/CommandHandler.cs
@@ -10,11 +10,14 @@ namespace HotTips.Engine
     [Name("HotTips command listener")]
     class CommandHandler :
         ICommandHandler<CommentSelectionCommandArgs>,
-        ICommandHandler<ToggleCompletionModeCommandArgs>
+        ICommandHandler<ToggleCompletionModeCommandArgs>,
+        ICommandHandler<TypeCharCommandArgs>
     {
         public string DisplayName => "HotTips command listener";
 
-        // Always use Unspecified state so that we don't interfere with regular execution
+        // Use CommandState.Unspecified to not interfere with execution, 
+        // but if there are not command handlers that mark this command as Available,
+        // then we won't run
         public CommandState GetCommandState(ToggleCompletionModeCommandArgs args) => CommandState.Unspecified;
 
         public bool ExecuteCommand(CommentSelectionCommandArgs args, CommandExecutionContext executionContext)
@@ -29,6 +32,13 @@ namespace HotTips.Engine
         {
             // Record that user used the Ctrl+Alt+Space command to use Suggestion Mode
             return false; // If we return true, the command is marked as "handled" and not actually executed
+        }
+
+        public CommandState GetCommandState(TypeCharCommandArgs args) => CommandState.Unspecified;
+
+        public bool ExecuteCommand(TypeCharCommandArgs args, CommandExecutionContext executionContext)
+        {
+            return false;
         }
     }
 }
